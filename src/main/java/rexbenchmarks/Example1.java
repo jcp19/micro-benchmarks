1ba19335a370e694d0ddef7c301767a2eb6bbc52
Joao Pereira <joaopereira.19@gmail.com>
2018-02-16 10:17:56 +0000

    fixed wrongly shared locks in Example1

--- src/main/java/rexbenchmarks/Example1.java
+++ src/main/java/rexbenchmarks/Example1.java
@@ -7,19 +7,17 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Example1 implements IExample{
     public void mainMinha(String[] args){
-        Lock lock = new ReentrantLock();
         Counter c = new Counter();
-        Thread1E1 t1 = new Thread1E1(lock, c);
-        Thread2E1 t2 = new Thread2E1(lock, c);
+        Thread1E1 t1 = new Thread1E1(new ReentrantLock(), c);
+        Thread2E1 t2 = new Thread2E1(new ReentrantLock(), c);
         t1.start();
         t2.start();
     }
 
     public static void main(String[] args) {
-        Lock lock = new ReentrantLock();
         Counter c = new Counter();
-        Thread1E1 t1 = new Thread1E1(lock, c);
-        Thread2E1 t2 = new Thread2E1(lock, c);
+        Thread1E1 t1 = new Thread1E1(new ReentrantLock(), c);
+        Thread2E1 t2 = new Thread2E1(new ReentrantLock(), c);
         t1.start();
         t2.start();
     }
