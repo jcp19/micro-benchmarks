90eaea73fc4b8f5ef5363a406daaa4555fc060be
Joao Pereira <joaopereira.19@gmail.com>
2018-02-20 17:12:10 +0000

    added example 3

--- src/main/java/rexbenchmarks/Example3.java
+++ src/main/java/rexbenchmarks/Example3.java
@@ -1,21 +1,38 @@
 package rexbenchmarks;
 
-public class Example3 {
-    public static void main(String[] args) {
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
+public class Example3 implements IExample {
+    public void mainMinha(String[] args) {
+        Lock lock = new ReentrantLock();
+        Counter c = new Counter();
+
+        Thread1E3 t1 = new Thread1E3(lock, c);
+        Thread2E3[] t2 = new Thread2E3[10];
+        for(int i = 0; i < 10; i++) {
+           t2[i] = new Thread2E3(lock, c, i+1);
+        }
+        t1.start();
+        for(int i = 0; i < 10; i++) {
+            t2[i].start();
+        }
+    }
+
+public static void main(String[] args) {
+        Lock lock = new ReentrantLock();
+        Counter c = new Counter();
+
+        Thread1E3 t1 = new Thread1E3(lock, c);
+        Thread2E3[] t2 = new Thread2E3[10];
+        for(int i = 0; i < 10; i++) {
+           t2[i] = new Thread2E3(lock, c, i+1);
+        }
+        t1.start();
+        for(int i = 0; i < 10; i++) {
+            t2[i].start();
+        }
     }
 }
 
-class Thread1E3 extends Thread {
-    @Override
-    public void run() {
-
-    }
-}
-
-class Thread2E3 extends Thread {
-    @Override
-    public void run() {
-        super.run();
-    }
-}
