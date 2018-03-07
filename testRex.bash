6350f05d8ceda9bc7d5887b1249b029bd4afe046
Joao Pereira <joaopereira.19@gmail.com>
2018-03-07 15:22:10 +0000

    removed portuguese word

--- testRex.bash
+++ testRex.bash
@@ -2,7 +2,7 @@
 for line in $(cat files2test.txt | grep -v "^[[:space:]]*$" files2test.txt | grep -v "^#.*"); do 
   echo ">>> Benchmark: $line";
   java -cp .:target/minha-benchmarks-1.0-SNAPSHOT-jar-with-dependencies.jar $line > /dev/null
-  echo ">>> Resultados:"
+  echo ">>> Results:"
   cd ../minha-checker
   bash ../minha-checker/tester.bash 
   cd - > /dev/null
