38e09477d678ceca2764dc3cc061343c86c86ac2
nunomachado <nuno.machado@ist.utl.pt>
2018-03-08 11:11:17 +0000

    Fix missing static keyword in class Example0

--- src/main/java/rexbenchmarks/Example0.java
+++ src/main/java/rexbenchmarks/Example0.java
@@ -2,7 +2,7 @@ package rexbenchmarks;
 
 public class Example0 {
 
-    public void main(String[] args){
+    public static void main(String[] args){
         Counter c = new Counter();
         Example0Thread t1 = new Example0Thread(c);
         Example0Thread t2 = new Example0Thread(c);
